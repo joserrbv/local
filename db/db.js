6872e8ae875976bb0811e4275862a975d074c4f8
Jose Queiroz <118081152+joserrbv@users.noreply.github.com>
2024-03-30 12:39:19 -0400

    adicionado .env

--- db/db.js
+++ db/db.js
@@ -4,12 +4,14 @@ require("dotenv").config();
 async function main() {
   mongoose.set("strictQuery", true);
 
-  const NAME = 'josequeiroz'
-  const SENHA = 'B3xkMtyq3M1PTcOD'
+
+  const USER = process.env.USER
+
+  const SENHA = process.env.SENHA
   
 
         try {
-            await mongoose.connect(`mongodb+srv://${NAME}:${SENHA}@cluster0.raiai7q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)          
+            await mongoose.connect(`mongodb+srv://${USER}:${SENHA}@cluster0.raiai7q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)          
                 console.log("conectado ao banco");
         } catch (error) {
                 console.log("erro de conexão!");
